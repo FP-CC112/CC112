3a38db58d09304af2a69c99b8a3fc0fdaa85598d
AmericoCh <americochulluncuy@gmail.com>
2025-03-25 15:20:11 -0500

    lab125031520

---
+++ Lab1/p2.cpp
@@ -0,0 +1,18 @@
+﻿// Implementa una función llamada cuadrado que tome un número entero como paráme-
+// tro y devuelva su cuadrado.
+#include <iostream>
+using namespace std;
+
+void cuadrado(int &a){
+    a = a*a;
+}
+
+int main (){
+    int a;
+    cout << "Escriba un número entero:"; cin >> a;
+
+    cuadrado(a);
+    cout << "El cuadrado del número es: " << a << endl; 
+
+     return 0;
+}
---
+++ Lab1/p4.cpp
@@ -0,0 +1,31 @@
+﻿// Implementa una función llamada esPrimo que determine si un número dado es primo.
+// La función debe devolver un valor booleano
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+bool esPrimo(int n){
+    if(n < 2)
+        return false;
+    
+    for(int i=2; i<=sqrt(n); i++){
+        if(n%i==0){
+            return  false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cout<<"ingrese un valor entero: ";
+    cin>>n;
+    if(esPrimo(n))
+    {
+        cout<<"el numero es primo";
+    }
+    else
+    cout<<"el numero no es primo";
+    
+    return 0; 
+}
