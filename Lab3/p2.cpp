1cba700765fefd3788c7299c4b0dda7e67473cd7
AmericoCh <americochulluncuy@gmail.com>
2025-04-08 15:48:23 -0500

    resueltos pd3

---
+++ Lab3/p2.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using namespace std;
+
+int busquedaBinaria(int array[], int n, int x){
+    int inicio=0;
+    int fin=n-1;
+    int medio;
+
+    while(inicio <= fin){
+        medio = inicio+(fin-inicio)/2;
+        if(array[medio]==x){
+            return medio;
+        } else if (array[medio]< x){
+            inicio = medio + 1;
+        } else {
+            fin = medio - 1;
+        }
+    }
+
+    return -1;
+}
+
+void ingresarDatos(int par[], int impar[], int n){
+    cout << "Ingrese los n elementos:\n";
+    for(int i = 0; i < n; ++i){
+        if(i % 2 == 0)
+            cin >> par[i/2];
+        else
+            cin >> impar[i/2];
+    }
+}
+
+
+
+
+int main(){
+
+    // 4 10 7 9 15 8 24 6 35 4 40 3 50 2 55 -5      n = 16
+
+    int arr[30];
+    int par[15];
+    int impar[15];
+    int n;
+    int x;
+
+    cout << "Ingrese el valor de n: ";
+    cin >> n;
+
+    ingresarDatos(par, impar, n);
+
+    cout << "Ingrese el valor a buscar";
+    cin >> x;
+
+    int  rpar = busquedaBinaria(par,n/2 + n%2 ,x);
+    int rimpar = busquedaBinaria(impar, n/2 ,x);
+
+    if(rpar != -1){
+        cout << "El elemento se encuentra en el indice " << rpar*2 <<endl;
+    }else if (rimpar != -1){
+        cout << "El elemento se encuentra en el indice " << rimpar*2+1 <<endl;
+    }else{
+        cout << "El elemento no se encuentra en el arreglo";
+    }
+
+    return 0; 
+}
---
+++ Lab3/p3.cpp
@@ -0,0 +1,71 @@
+﻿/*Dado dos arreglos arr1[] y arr2[] de números enteros ordenados en forma ascendente,
+cuyos tamaños son n1 y n2 respectivamente (tamaño máximo 20). Escribe una función
+llamada mezcla que fusione ambos arreglos en un solo arreglo ordenado. Ejemplo
+Entrada:
+arr1[] = {1, 4, 6, 9}
+arr2[] = {2, 3, 7, 8, 10}
+Salida:
+arr[] = {1, 2, 3, 4, 6, 7, 8, 9, 10}*/
+
+#include <iostream>
+using namespace std;
+
+
+//revisar funcion
+void merge(int arr1[], int n1, int arr2[], int n2, int arr[]){
+    int i = 0, j = 0 , k = 0;
+
+    while(i < n1 && j < n2){
+        // if(arr1[i] < arr2[j]){
+        //     arr[k] = arr1[i];
+        //     i++;
+        // }else{
+        //     arr[k] = arr2[j];
+        //     j++;
+        // }
+        // k++;
+        arr[k++] = (arr1[i] < arr2[j]) ? arr1[i++] : arr2[j++];
+    }
+
+    while(i < n1){
+        arr[k] = arr1[i];
+        i++;
+        k++;
+    }
+
+    while(j < n2){
+        arr[k] = arr2[j];
+        j++;
+        k++;
+    }
+
+
+}
+
+void imprimirArreglo(int a[], int n){
+    for(int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int arr[40];
+
+    int arr1[] = {1, 4, 6, 9};
+    int arr2[] = {2, 3, 7, 8, 10};
+
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
+
+    merge(arr1, n1, arr2, n2, arr);
+
+    imprimirArreglo(arr,n1+n2);
+
+
+
+
+
+
+    return 0; 
+}
