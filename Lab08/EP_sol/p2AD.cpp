579a9017056f4df67c66d1ece3d40ada4067e287
AmericoCh <americochulluncuy@gmail.com>
2025-05-27 14:52:47 -0500

    sol2, 3

--- Lab08/EP_sol/p2AD.cpp
+++ Lab08/EP_sol/p2AD.cpp
@@ -1,12 +1,12 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 //const int MAX = 10;
 
-void Menor(double (*M)[MAX], double (*A)[MAX], int n , int filEliminar, int colEliminar){
+void Menor(double **M, double **A, int n , int filEliminar, int colEliminar){
     int filM = 0;
     for (int i = 0; i < n; ++i) {
         if ( i != filEliminar) {
@@ -24,12 +24,27 @@ void Menor(double (*M)[MAX], double (*A)[MAX], int n , int filEliminar, int colE
     }
 }
 
-double determinante(double (*A)[MAX],  int n) {
+void eliminarMatriz (double **M, int n){
+    for (int i = 0 ; i < n; ++i) {
+        delete[] M[i];
+    }
+    delete[] M;
+}
+
+double determinante(double **A,  int n) {
     //caso base
     if (n == 1) 
         return **A;
     
-    double M[MAX][MAX];
+
+    
+    //double M[MAX][MAX];
+    // asignacion dinamica para M
+    double **M  = new double*[n-1]; 
+
+    for(int i = 0 ; i < n-1; ++i) {
+        M[i] = new double[n-1];
+    }
 
     //por ahora i = 0;
 
@@ -40,16 +55,43 @@ double determinante(double (*A)[MAX],  int n) {
         Menor(M, A, n, 0, j);
         det += pow(-1, 0 + j) * A[0][j]*determinante(M, n-1); 
     }
+    eliminarMatriz(M, n-1);
     return det;
 }
 
 int main(){
     int n = 2;
-    double A[MAX][MAX] = {{1,0},{0,1}};
+
+    double **A  = new double*[n]; 
+
+    for(int i = 0 ; i < n; ++i) {
+        A[i] = new double[n];
+    }
+
+    srand(time(0));
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            A[i][j] = rand() % 2;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+
+
 
     double resultado = determinante(A,n);
 
     cout << resultado;
 
+    eliminarMatriz(A,n);
+    
     return 0; 
 }
---
+++ Lab08/EP_sol/p3.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+const int MAX = 100;
+int mapa[MAX][MAX];
+bool visitado[MAX][MAX];
+int filas, columnas;
+
+void dfs(int i, int j) {
+    if(i < 0 || i >= filas || j < 0 || j >=columnas)
+        return;
+
+    if (mapa[i][j] == 1 || visitado[i][j])
+        return;
+    visitado[i][j] = true;
+    dfs(i, j-1);
+    dfs(i, j+1);
+    dfs(i-1, j);
+    dfs(i+1, j);
+    
+}
+
+
+int contRegionesConexas() {
+    int contRegiones = 0;
+    for (int i = 0; i < filas; ++i) {
+        for (int j = 0; j < columnas; ++j) {
+            if (mapa[i][j] == 0 && !visitado[i][j]) {
+                dfs(i,j);
+                ++contRegiones;
+            }
+        }
+    }
+    return contRegiones;
+}
+
+int main(){
+    filas = 4;
+    columnas = 5;
+    int datos[4][5] = {
+        {1, 0, 0, 1, 1},  
+        {1, 0, 1, 1, 0},  
+        {1, 1, 1, 0, 0},  
+        {0, 1, 1, 1, 1} 
+    };
+
+    //copiar
+    for (int i = 0; i < filas; ++i) {
+        for (int j = 0; j < columnas; ++j) {
+            mapa[i][j] = datos[i][j];
+        }
+    }
+
+    cout << "Cantidad de regiones conexas:  " << contRegionesConexas() <<endl;
+
+
+    return 0; 
+}
